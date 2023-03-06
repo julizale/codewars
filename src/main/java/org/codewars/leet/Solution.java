8b117ecced1e10f0f5b10be39359bd10291c3cd3
Julian Zaleski <julizale@gmail.com>
2023-03-06 10:15:46 +0100

    Longest substring

---
+++ src/main/java/org/codewars/leet/Solution.java
@@ -0,0 +1,23 @@
+package org.codewars.leet;
+
+class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        StringBuilder sub = new StringBuilder();
+        String longestSub = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (sub.toString().indexOf(s.charAt(i)) == -1) {
+                sub.append(s.charAt(i));
+            } else {
+                if (sub.length() > longestSub.length()) {
+                    longestSub = sub.toString();
+                }
+                sub = new StringBuilder();
+            }
+        }
+        if (sub.length() > longestSub.length()) {
+            longestSub = sub.toString();
+        }
+        return longestSub.length();
+    }
+}
---
+++ src/test/java/org/codewars/leet/SolutionTest.java
@@ -0,0 +1,19 @@
+package org.codewars.leet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void testLengthOfLongestSubstring() {
+        //Given
+        Solution solution = new Solution();
+
+        //When & Then
+        assertEquals(3, solution.lengthOfLongestSubstring("abcabcbb"));
+        assertEquals(1, solution.lengthOfLongestSubstring("bbbbb"));
+        assertEquals(3, solution.lengthOfLongestSubstring("pwwkew"));
+    }
+}
