ba21b02734248722309099c8ae9be825f8eee815
Julian Zaleski <julizale@gmail.com>
2023-03-06 12:30:48 +0100

    Roman to integer

--- src/main/java/org/codewars/leet/Solution.java
+++ src/main/java/org/codewars/leet/Solution.java
@@ -21,5 +21,45 @@ class Solution {
         }
         return longestSub.length();
     }
+
+    public int romanToInt(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'I') {
+                if (i < s.length() - 1 && (s.charAt(i+1) == 'V' || s.charAt(i+1) == 'X')) {
+                    result -= 1;
+                } else {
+                    result += 1;
+                }
+            }
+            if (s.charAt(i) == 'V') {
+                result += 5;
+            }
+            if (s.charAt(i) == 'X') {
+                if (i < s.length() - 1 && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C')) {
+                    result -= 10;
+                } else {
+                    result += 10;
+                }
+            }
+            if (s.charAt(i) == 'L') {
+                result += 50;
+            }
+            if (s.charAt(i) == 'C') {
+                if (i < s.length() - 1 && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M')) {
+                    result -= 100;
+                } else {
+                    result += 100;
+                }
+            }
+            if (s.charAt(i) == 'D') {
+                result += 500;
+            }
+            if (s.charAt(i) == 'M') {
+                result += 1000;
+            }
+        }
+        return result;
+    }
 }
 
--- src/test/java/org/codewars/leet/SolutionTest.java
+++ src/test/java/org/codewars/leet/SolutionTest.java
@@ -18,4 +18,15 @@ class SolutionTest {
         assertEquals(2, solution.lengthOfLongestSubstring("aab"));
         assertEquals(3, solution.lengthOfLongestSubstring("dvdf"));
     }
+
+    @Test
+    void testRomanToInt() {
+        //Given
+        Solution solution = new Solution();
+
+        //When & Then
+        assertEquals(3, solution.romanToInt("III"));
+        assertEquals(58, solution.romanToInt("LVIII"));
+        assertEquals(1994, solution.romanToInt("MCMXCIV"));
+    }
 }
